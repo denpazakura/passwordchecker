263d80ab3f9352ce6fe5dfc66d1d636a46d6f4c7
denpazakura <denpazakura@gmail.com>
2024-02-12 15:58:34 +0100

    Replaced hash iteration with binary search

--- passwordchecker.py
+++ passwordchecker.py
@@ -1,8 +1,8 @@
 import requests
 import hashlib
 import sys
+import bisect
 
-# 'CBFDA'
 def request_api_data(query_param):
     url = 'https://api.pwnedpasswords.com/range/' + query_param
     res = requests.get(url)
@@ -19,21 +19,25 @@ def pwned_api_check(password):
     response = request_api_data(head)
     return get_password_leak_count(response, tail)
 
-def get_password_leak_count(hashes, hash_to_check):
-    hashes = (line.split(':') for line in hashes.text.splitlines())
-    
-    for h, count in hashes:
-        if h == hash_to_check:
-            return count
+def binary_search(hashes, hash_to_check):
+    hash_values = [hash_tuple[0] for hash_tuple in hashes]
+    index = bisect.bisect_left(hash_values, hash_to_check)
+    if index < len(hashes) and hashes[index][0] == hash_to_check:
+        return hashes[index][1]
     return 0
+
+def get_password_leak_count(hashes, hash_to_check):
+    hashes = [line.split(':') for line in hashes.text.splitlines()]
+    hashes.sort()
+    return binary_search(hashes, hash_to_check)
 
 def main(args):
      for password in args:
          count = pwned_api_check(password)
          if count!=0:
-            print(f'{password} was found {count} times... you should probably change your password!')
+            print(f'{password} was found {count} times, consider another password')
          else:
-            print(f'{password} was NOT found. Carry on!')
+            print(f'{password} was not found and may be considered safe to use')
 
 if __name__ == '__main__':
   sys.exit(main(sys.argv[1:]))
